f12a279db2f367bcfef4d80cc27bd4cb385c0550
Nathan <almeida.nathan92@gmail.com>
2023-03-15 17:07:04 -0300

    add player dmg

---
+++ Assets/Scripts/Player/PlayerDmg.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDmg : MonoBehaviour
+{
+    [SerializeField] private PlayerStats pStats;
+    [SerializeField] private Player player;
+
+    [SerializeField] private float kbForce;
+    [SerializeField] private float defMultiplier;
+
+    public void TakeDamage(float atk, bool blocked, Vector3 enemyPos)
+    {
+        if (atk < pStats.def)
+        {
+            pStats.hp -= 1;
+        }       
+        else
+        {
+            float def = (pStats.def * defMultiplier) / 100;
+            float dmg = atk - def;
+
+            pStats.hp -= (dmg - pStats.def);
+        }
+        
+        if (pStats.hp <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            Vector3 kbDir = (transform.position - enemyPos).normalized;
+            Knockback(kbDir);
+        }
+    }
+
+    private void Die()
+    {
+        player.Anim.SetTrigger("Hit");
+        player.Anim.SetBool("Death", true);
+    }
+
+    private void Knockback(Vector3 kbDir)
+    {
+        player.Anim.SetTrigger("Hit");
+        player.Rig.AddForce(kbDir * kbForce, ForceMode.Impulse); 
+    }
+}
